fc40dff442cf869798a38897b44fd2479455bbd1
M@W <mehdicr7wahid@gmail.com>
2024-03-03 15:51:04 +0430

    updated UI

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -3,15 +3,9 @@
 import { cx } from "class-variance-authority";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Dispatch, SetStateAction } from "react";
-import { MdOutlineLightMode } from "react-icons/md";
 import { Button } from "../molecules/button";
 
-export default function Navbar({
-  setTheme,
-}: {
-  setTheme?: Dispatch<SetStateAction<boolean>>;
-}) {
+export default function Navbar() {
   const pathname = usePathname();
 
   const Download = async () => {
@@ -46,7 +40,7 @@ export default function Navbar({
   };
   return (
     <div className="col-span-4  mx-4 mt-8 flex items-center justify-between font-bebas_neve lg:col-start-2  lg:col-end-12 lg:mx-0 ">
-      <div className=" text-[2.5rem] tracking-[0.05em] ">M@W</div>
+      <div className="  text-[2.5rem] tracking-[0.05em] ">M@W</div>
       <div className="flex items-center space-x-8 font-helvetica text-lg capitalize ">
         <Button
           label="Resume"
@@ -70,13 +64,6 @@ export default function Navbar({
         >
           about
         </Link>
-
-        {/* <MdOutlineLightMode
-          size={24}
-          onClick={() => setTheme && setTheme((theme: boolean) => !theme)}
-          className="cursor-pointer hover:scale-105"
-          title="Dark Theme"
-        /> */}
       </div>
     </div>
   );
