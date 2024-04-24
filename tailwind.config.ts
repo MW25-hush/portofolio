7fb71b22fa0cf09e68b966a233b0c3633e0dd32a
M@W <mehdicr7wahid@gmail.com>
2024-04-24 19:57:57 +0430

    removed unnecessary files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,29 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#0A4D15",
-        secondary: "#D2FBD0",
-        tertiary: "#000000",
-        accent: {
-          gray: "#96A0A6",
-          lightGray: "#F9F9F9",
-        },
-      },
-      fontFamily : {
-        bebas_neve: ['var(--font-bebas-neve)'],
-        helvetica : ["Helvetica"]
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-
-    },
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
